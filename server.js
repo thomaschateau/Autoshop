75fb621471cf542b419b8dfbcc9f4ef292a0fc98
martinikei <m.jedrol@yahoo.com>
2018-04-19 19:43:32 +0100

    page

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 // index page
 app.get('/', function(req, res) {
- res.render('home/codio/workspace/AutoshopWeb/views/pages/index');
+ res.render('pages/index');
 });
 // about page
 app.get('/about', function(req, res) {
