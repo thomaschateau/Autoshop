217c7a70c64ecdfd49680ff29e344506a74e85b3
martinikei <m.jedrol@yahoo.com>
2018-04-19 17:03:59 +0100

    asd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var knockknock = require('knock-knock-jokes');
 http.createServer(function (req, res) {
- res.writeHead(200, {'Content-Type': ‘text/html'});
+ res.writeHead(200, {'Content-Type': 'text/html'});
 var randomJoke = knockknock()
  res.end(randomJoke);
 }).listen(8080);
