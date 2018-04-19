b2464e3b14c07908bb79018876dd8d11b4663398
martinikei <m.jedrol@yahoo.com>
2018-04-20 00:56:03 +0100

    asd

--- server.js
+++ server.js
@@ -9,8 +9,17 @@ var app = express();
 app.set('view engine', 'ejs');
 // render pages
 app.get('/', function(req, res) {
- res.render('pages/index');
-});
+  var drinks = [
+  { name: 'Bloody Mary', drunkness: 3 },
+  { name: 'Martini', drunkness: 5 },
+  { name: 'Scotch', drunkness: 10 }
+  ];
+  var tagline = "Any code of your own that you havent looked at for six or more months might as well have been written by someone else."
+  res.render('pages/index', {
+  drinks: drinks,
+  tagline: tagline
+  });
+ });
 app.get('/about', function(req, res) {
  res.render('pages/about');
 });
