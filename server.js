ff7aea0cd3e45c3645a6f5844e0e0c0a137451e3
martinikei <m.jedrol@yahoo.com>
2018-04-19 22:45:16 +0100

    asd

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ var express = require('express');
 var app = express();
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-// use res.render to load up an ejs view file
-// pages
-// index page
+// render pages
 app.get('/', function(req, res) {
  res.render('pages/index');
 });
