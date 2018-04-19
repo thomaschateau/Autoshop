69016fe5e03250693ed8cd489705a9107681a4dd
martinikei <m.jedrol@yahoo.com>
2018-04-19 17:35:24 +0100

    asd

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'))
-app.get('/', function(req, res){
- res.send("Hello world! by express");
+app.get('/getform', function(req, res){
+var name = req.query.name;
+var quest = req.query.quest;
+ res.send("Hi "+name+" I am sure you will "+quest) ;
 });
 app.listen(8080);
