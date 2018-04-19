9d2636f19164ef2edec1826f7247771626abc107
martinikei <m.jedrol@yahoo.com>
2018-04-19 17:13:04 +0100

    asd

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.get('/', function(req, res){
+app.get('/test', function(req, res){
  res.send("Hello world! by express");
 });
 app.listen(8080);
