90465641ae4e2f56dbce345bc7769e21da9324f6
martinikei <m.jedrol@yahoo.com>
2018-04-19 17:25:18 +0100

    aa

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 app.use(express.static('public'))
 var app = express();
-app.get('/public', function(req, res){
+app.get('/', function(req, res){
  res.send("Hello world! by express");
 });
 app.listen(8080);
