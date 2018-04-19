d0ee9bb7c1d985c198f782d1fb5a283c12b5c8f4
martinikei <m.jedrol@yahoo.com>
2018-04-19 16:43:41 +0100

    more server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
+var currentdate = require('./module1');
 http.createServer(function (req, res) {
  res.writeHead(200, {'Content-Type': 'text/html'});
+ res.write("The date and time are currently: " + currentdate.myDateTime());
  res.end('Hello World!');
 }).listen(8080);
