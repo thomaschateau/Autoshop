9847140a34c8a9c8f1c1bf5ad4c8f66da4c1b604
martinikei <m.jedrol@yahoo.com>
2018-04-19 21:50:52 +0100

    more changes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 
+console.log('Node server by Autograph.');
+console.log('Initialising...');
 // server.js
 // load the things we need
 var express = require('express');
@@ -6,13 +8,28 @@ var app = express();
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
+// pages
 // index page
 app.get('/', function(req, res) {
  res.render('pages/index');
 });
-// about page
 app.get('/about', function(req, res) {
  res.render('pages/about');
 });
+app.get('/contact', function(req, res) {
+ res.render('pages/contact');
+});
+app.get('/login', function(req, res) {
+ res.render('pages/login');
+});
+app.get('/register', function(req, res) {
+ res.render('pages/register');
+});
+app.get('/basket', function(req, res) {
+ res.render('pages/basket');
+});
 app.listen(8080);
+console.log('All good to go!');
+console.log(' ');
 console.log('Hit ctrl + c to shut down the server.');
+console.log(' ');
