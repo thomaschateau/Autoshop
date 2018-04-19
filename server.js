a1d81bfce9a6288135e1d5b2ad1ca005438676ba
martinikei <m.jedrol@yahoo.com>
2018-04-19 17:38:41 +0100

    asd

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-app.use(express.static('public'))
+app.use(express.static('AutoshopWeb/public'))
+
 app.get('/getform', function(req, res){
 var name = req.query.name;
 var quest = req.query.quest;
