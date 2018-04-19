557f34cd296e509623add42d8c7fca70e4b3da40
martinikei <m.jedrol@yahoo.com>
2018-04-19 17:01:55 +0100

    asd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
-var currentdate = require('./module1');
+var knockknock = require('knock-knock-jokes');
 http.createServer(function (req, res) {
- res.writeHead(200, {'Content-Type': 'text/html'});
- res.write("The date and time are currently: " + currentdate.myDateTime());
- res.end('Hello World!');
+ res.writeHead(200, {'Content-Type': ‘text/html'});
+var randomJoke = knockknock()
+ res.end(randomJoke);
 }).listen(8080);
