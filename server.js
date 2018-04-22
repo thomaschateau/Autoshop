c6751e21d1dceab7d07ddf041d9d52c56c4869b2
martinikei <m.jedrol@yahoo.com>
2018-04-22 16:00:34 +0100

    asd

--- server.js
+++ server.js
@@ -49,6 +49,11 @@ app.get('/basket', function(req, res) {
  res.render('pages/basket');
 });
 
+app.get('/getform', function(req, res){
+var name = req.query.name;
+var quest = req.query.quest;
+ res.send("Hi "+name+" I am sure you will "+quest) ;
+});
 
 app.get('/item', function(req, res){
 var x = req.query.x;
