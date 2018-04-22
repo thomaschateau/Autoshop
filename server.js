f7f9f8272425d427006f6fdb59113cbfc5df608a
martinikei <m.jedrol@yahoo.com>
2018-04-22 15:46:20 +0100

    sdf

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get('/basket', function(req, res) {
 app.get('/item', function(req, res){
 var x = req.query.x;
 var y = req.query.y;
- res.send("X + Y="+(x+y));
+ res.send("X + Y=" + parseInt(x+y));
 });
 
 
