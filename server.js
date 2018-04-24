7cb1997acc90466bcc270c7956e20b307f4a07a3
martinikei <m.jedrol@yahoo.com>
2018-04-25 00:50:22 +0100

    asd

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ var datatostore = {
 //once created we just run the data string against the database and all our new data will be saved/
   db.collection('people').save(datatostore, function(err, result) {
     if (err) throw err;
-    console.log('saved to database')
+    console.log('saved' + datatostore)
     //when complete redirect to the index
     res.redirect('/login')
   })
