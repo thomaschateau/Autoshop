172033611d7224b05fd7a282469b0ede8c7dc29a
martinikei <m.jedrol@yahoo.com>
2018-04-24 22:38:13 +0100

    ss

--- server.js
+++ server.js
@@ -53,7 +53,9 @@ app.get('/', function(req, res) {
   db.collection('people').find().toArray(function(err, result) {
     if (err) throw err;
     //the result of the query is sent to the users page as the "users" array
-    res.redirect('/users')
+    res.render('pages/users', {
+      users: result
+    })
   });
 
 });
