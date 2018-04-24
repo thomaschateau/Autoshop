dce618af4f51d64f9a0f30378521e54099fc8da4
martinikei <m.jedrol@yahoo.com>
2018-04-24 23:11:13 +0100

    sss

--- server.js
+++ server.js
@@ -50,23 +50,13 @@ app.get('/', function(req, res) {
   if(!req.session.loggedin){res.redirect('/index');return;}
 
   //otherwise perfrom a search to return all the documents in the people collection
-  db.collection('people').find().toArray(function(err, result) {
+  db.collection('people').findOne({"login.username":uname}, function(err, result) {
+    var uname = req.query.username;
     if (err) throw err;
     //the result of the query is sent to the users page as the "users" array
-    //get the requested user based on their username, eg /profile?username=dioreticllama
-    var uname = req.query.username;
-    //this query finds the first document in the array with that username.
-    //Because the username value sits in the login section of the user data we use login.username
-    db.collection('people').findOne({
-      "login.username": uname
-    }, function(err, result) {
-      if (err) throw err;
-      //console.log(uname+ ":" + result);
-      //finally we just send the result to the user page as "user"
-      res.render('pages/profile', {
-        user: result
-      })
-    });
+    res.render('pages/users', {
+      users: result, uname : uname
+    })
   });
 
 });
