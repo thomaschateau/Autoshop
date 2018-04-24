f228185ceb3d15f3a7e3822875956b8497ba70d6
martinikei <m.jedrol@yahoo.com>
2018-04-24 01:32:39 +0100

    asd

--- server.js
+++ server.js
@@ -61,8 +61,23 @@ var quest = req.query.quest;
  res.send("Hi "+name+" I am sure you will "+quest) ;
 });
 
+app.get('/trousers', function(req, res) {
+  db.collection('trousers').find().toArray(function(err, result) {
+  if (err) throw err;
+  var trousers = [];
+  for (var i = 0; i < result.length; i++) {
+    trousers.push({"sku": result[i].sku,
+     "brand": result[i].brand,
+      "type": result[i].type,
+      "description": result[i].description,
+     "img_holder": '<a href="/itm_trousers?sku=' + result[i].sku + '"><img class="card-img-top" src="../' + result[i].sku + '.png" alt="' + result[i].sku + '.png"></a>'});
+    //console.log(path);
+  }
+  res.render('pages/trousers', { trousers: trousers});
+});
+});
 app.get('/itm_trousers', function(req, res){
-console.log(req.query.sku);
+//console.log(req.query.sku);
 db.collection('trousers').find({sku: req.query.sku}).toArray(function(err, result) {
 if (err) throw err;
 var search = [];
@@ -86,29 +101,37 @@ app.get('/promotions', function(req, res) {
   if (err) throw err;
   var promotions = [];
   for (var i = 0; i < result.length; i++) {
-    promotions.push({"sku": result[i].sku, "brand": result[i].brand, "type": result[i].type, "description": result[i].description, "path": "/" + result[i].sku + ".jpg"});
+    promotions.push({"sku": result[i].sku,
+     "brand": result[i].brand,
+      "type": result[i].type,
+       "description": result[i].description,
+      "img_holder": '<a href="/itm_promo?sku=' + result[i].sku + '"><img class="card-img-top" src="../' + result[i].sku + '.png" alt="' + result[i].sku + '.png"></a>'});
     //console.log(path);
   }
   res.render('pages/promotions', { promotions: promotions });
 });
 });
+app.get('/itm_promo', function(req, res){
+//console.log(req.query.sku);
+db.collection('promotions').find({sku: req.query.sku}).toArray(function(err, result) {
+if (err) throw err;
+var search = [];
+  for (var i = 0; i < result.length; i++) {
+    search.push({"sku": result[i].sku,
+              "brand": result[i].brand,
+              "type": result[i].type,
+              "description": result[i].description,
+               "path": "/" + result[i].sku + ".png",
+               "colour": result[i].colour,
+               "quantity": result[i].quantity,
+               "price": result[i].price,
+               "img_holder": '<img class="card-img-top" src="../' + result[i].sku + '.png" alt="' + result[i].sku + '.png">'});
+  }
+res.render('pages/itm_promo', { search: search });
+});
+});
 
 console.log('....');
-app.get('/trousers', function(req, res) {
-  db.collection('trousers').find().toArray(function(err, result) {
-  if (err) throw err;
-  var trousers = [];
-  for (var i = 0; i < result.length; i++) {
-    trousers.push({"sku": result[i].sku,
-     "brand": result[i].brand,
-      "type": result[i].type,
-      "description": result[i].description,
-     "img_holder": '<a href="/itm_trousers?sku=' + result[i].sku + '"><img class="card-img-top" src="../' + result[i].sku + '.png" alt="' + result[i].sku + '.png"></a>'});
-    //console.log(path);
-  }
-  res.render('pages/trousers', { trousers: trousers});
-});
-});
 
 
 app.get('/tshirts', function(req, res) {
