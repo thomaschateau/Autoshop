01759b5b86de28808ae716903c06bb5ffc31e844
martinikei <m.jedrol@yahoo.com>
2018-04-25 02:23:22 +0100

    asd

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ MongoClient.connect(url, function(err, database) {
 
 app.get('/api', function(req, res) {
  var params = {
- screen_name: 'rgu'
+ screen_name: 'dev'
  };
  client.get('statuses/user_timeline', params, function(error, tweets, response) {
  if (!error) {
