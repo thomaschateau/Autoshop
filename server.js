e3b33638509d9a2d9a7755017176f4dfea237ecc
martinikei <m.jedrol@yahoo.com>
2018-04-25 02:35:16 +0100

    sd

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ MongoClient.connect(url, function(err, database) {
 
 app.get('/api', function(req, res) {
  var params = {
- screen_name: 'nodejs'
+ screen_name: 'TwitterDev'
  };
  client.get('statuses/user_timeline', params, function(error, tweets, response) {
  if (!error) {
